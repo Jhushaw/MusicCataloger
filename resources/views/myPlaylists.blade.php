5d28d7f911d3d9e2e2ddf52a3089d62a82caf815
Jacob Hushaw <Jshushaw@gmail.com>
2021-02-12 17:09:23 -0700

    edited playlist view

--- resources/views/myPlaylists.blade.php
+++ resources/views/myPlaylists.blade.php
@@ -1,41 +1,41 @@
-@extends('layouts.appmaster')
-
-@section('title','HomePage')
+@extends('layouts.appmaster') @section('title','HomePage')
 
 @section('content')
 <div class="containerfull">
-<form action="createplaylist" method="GET">
-	<input type="hidden" name="_token" value=" <?php echo csrf_token()?>" />
-	<h2>Create a Playlist</h2>
-	<table>
+	<form action="createplaylist" method="GET">
+		<input type="hidden" name="_token" value=" <?php echo csrf_token()?>" />
+		<h2>Create a Playlist</h2>
+		<table>
+			<tr>
+				<td colspan="2" align="center"><input type="submit"
+					class="btn btn-dark btn-lg" name="Create Playlist" /></td>
+			</tr>
+		</table>
+	</form>
+	@if(isset($playlists))
+	<table border="1">
 		<tr>
-			<td colspan="2" align="center"><input type="submit" class="btn btn-dark btn-lg" name="Create Playlist" /></td>
+			<th>NAME</th>
+		</tr>
+
+		@foreach ($playlists as $p)
+		<tr>
+			<td>{{ $p['NAME']}}</td>
+
+			<td><form action="viewPlaylist" method="post">
+					<input type="hidden" name="_token" value="{{ csrf_token()}}" /> <input
+						type="hidden" name="id" value="{{ $p['ID'] }}" /> <input
+						type="submit" class="btn btn-secondary btn-large"
+						value="View Playlist" />
+				</form></td>
 		</tr>
 	</table>
-</form>
-@if(isset($playlists))
-<table border="1">
-        <tr>
-            <th>NAME</th>
-        </tr>
-		
-        @foreach ($playlists as $p) 
-        <tr>
-            <td>{{ $p['NAME']}}</td>
-            
-            <td><form action ="viewPlaylist" method="post">
-            <input type="hidden" name="_token" value="{{ csrf_token()}}" />
-            <input type="hidden" name="id" value="{{ $p['ID'] }}"/>
-            <input type="submit" class="btn btn-secondary btn-large" value="View Playlist"/>
-            </form></td>
-            </tr>
-            </table>
          @endforeach
          @endif
          <?php
-         if (isset($msg)) {
-    echo $msg;
-}
-?></h5>
+        if (isset($msg)) {
+            echo $msg;
+        }
+        ?>
 </div>
 @endsection
